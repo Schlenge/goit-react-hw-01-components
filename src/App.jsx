ef976fcbf9e831430221baf6cfede8a7c4f9906a
Schlenge <emil.tarala@gmail.com>
2023-11-09 14:07:36 +0200

    commit 5

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,13 @@ import data from './components/Data/data.json';
 import friends from './components/Data/friends.json';
 import transactions from './components/Data/transactions.json';
 
-import styles from './App.module.css'; // Importați stilurile aici
+import styles from './App.module.css';
 
 export function App() {
   document.title = 'HW React 1';
   return (
     <div className={styles.app}>
       {' '}
-      {/* Aplicați stilurile la containerul principal */}
       <h1 className={styles.title}>React Homework 1 - Components</h1>
       <div className={styles.components}>
         <Profile user={user} />
